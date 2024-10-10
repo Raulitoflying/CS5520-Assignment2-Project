80c4d6483a233f4aee1fec0132451b4e2eff8d4c
Raulitoflying <1017329098@qq.com>
2024-10-10 15:50:19 -0700

    Refactor code structure and add navigation functionality

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AddAnActivity from './Screens/AddAnActivity';
+import AddADietEntry from './Screens/AddADietEntry';
+import Home from './Components/Home';
+import { colors, commonStyles } from './helper/color';
+import React, { useState } from 'react';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
+  const [activities, setActivities] = useState([]);
+  const [diet, setDiet] = useState([]);
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  const addAnActivity = (newActivity) => {
+    setActivities(prevActivity => [...prevActivity, newActivity]);
+  };
+
+  const addADietEntry = (newDietEntry) => {
+    setDiet(prevDietEntry => [...prevDietEntry, newDietEntry]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerStyle: commonStyles.navigatorBackground, headerTintColor: colors.white }}>
+        <Stack.Screen name='Home'>
+          {props => (
+            <Home
+              {...props}
+              theme={theme}
+              activities={activities}
+              diet={diet}
+              toggleTheme={toggleTheme}
+              addAnActivity={addAnActivity}
+              addADietEntry={addADietEntry}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name='AddAnActivity'>
+          {props => <AddAnActivity {...props} addAnActivity={addAnActivity} options={{ title: 'Add An Activity'}}/>}
+        </Stack.Screen>
+        <Stack.Screen name='AddADietEntry'>
+          {props => <AddADietEntry {...props} addADietEntry={addADietEntry} options={{ title: 'Add A Diet Entry'}}/>}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
