d0e5372b47a2b3a2aab0aeb0a065bbcb221180f2
Raulitoflying <1017329098@qq.com>
2024-10-10 17:49:39 -0700

    Refactor Activities screen to use common styles and improve performance

---
+++ Screens/Activities.js
@@ -0,0 +1,14 @@
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { commonStyles } from '../helper/helper';
+import ItemsList from '../Components/ItemsList';
+
+export default function Activities({ theme, activities }) {
+  return (
+    <View style={[commonStyles.topContainer, commonStyles[theme]]}>
+      <ItemsList items={activities} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
