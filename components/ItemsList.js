b6b9125c235a3ea5c3097fd0d4b6acff58fe302f
Raulitoflying <1017329098@qq.com>
2024-10-10 17:48:13 -0700

    Refactor ItemsList component to use FlatList for better performance

--- components/ItemsList.js
+++ components/ItemsList.js
@@ -2,10 +2,8 @@ import { FlatList, StyleSheet } from 'react-native'
 import React from 'react'
 import Item from './Item'
 
-// Use the prop "item" to communicate what type of entries (diet or activity) to show.
 export default function ItemsList({items}) {
   return (
-    // FlatList has a better performance for showing a list of dynamic data than ScrollView.
     <FlatList
         data={items}
         renderItem={({item}) => <Item item={item} />}
