29b1997f0de501eaec351542e4fb5dd1a1e8fbe5
Raulitoflying <1017329098@qq.com>
2024-10-10 23:24:29 -0700

    Refactor Diet component to remove unused import statement

--- Screens/Diet.js
+++ Screens/Diet.js
@@ -1,16 +1,13 @@
-
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import { commonStyles } from '../helper/helper';
 import ItemsList from '../Components/ItemsList';
 
 export default function Diet({ theme, diet }) {
-
   return (
     <View style={[commonStyles.topContainer, commonStyles[theme]]}>
       <ItemsList items={diet} />
     </View>
-
   );
 }
 
