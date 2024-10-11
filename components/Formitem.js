4d5e4f2eb50a6ec0f64943684f700d3e43fae6ed
Raulitoflying <1017329098@qq.com>
2024-10-10 17:47:37 -0700

    Refactor FormItem component to pass theme prop

--- components/Formitem.js
+++ components/Formitem.js
@@ -1,10 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useContext } from 'react'
 import { commonStyles } from '../helper/helper'
-import { Context } from '../helper/context';
 
-export default function FormItem({label, children}) {
-  const { theme } = useContext(Context);
+
+export default function FormItem({label, children, theme}) {
 
   return (
     <View style={commonStyles.item}>
