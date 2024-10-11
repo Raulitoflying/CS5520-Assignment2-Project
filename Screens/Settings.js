3a31653af8cb8be16207b1ca6741ab416c3ee829
Raulitoflying <1017329098@qq.com>
2024-10-10 22:50:24 -0700

    Refactor Settings screen to remove merge conflicts and improve code structure

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 import { commonStyles } from '../helper/helper';
@@ -12,26 +11,3 @@ export default function Settings({ theme, toggleTheme }) {
 }
 
 const styles = StyleSheet.create({});
-=======
-import { Button, StyleSheet, View } from 'react-native'
-import React, { useContext } from 'react'
-import { colors, commonStyles } from '../Helper/helper'
-import { Context } from '../App';
-
-export default function Settings() {
-  // Use React Context to communicate theme change to all the components.
-  const { theme, toggleTheme } = useContext(Context);
-
-  return (
-    <View style={[commonStyles.centerContainer, commonStyles[theme]]}>
-      <Button
-        title='Toggle Theme'
-        onPress={toggleTheme}
-        color={theme === 'light' ? colors.bluishViolet : colors.light}
-      />
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({})
->>>>>>> 1d3df7abdaa7cea89f6780a9b026eefa66418eb1
