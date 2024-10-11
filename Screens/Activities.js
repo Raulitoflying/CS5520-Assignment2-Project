9945ce5d20bb224d06e06bab6d1180d57907b543
Raulitoflying <1017329098@qq.com>
2024-10-10 17:00:50 -0700

    Refactor Activities screen to remove unused imports and styles

---
+++ Screens/Activities.js
@@ -0,0 +1,18 @@
+import { StyleSheet, View } from 'react-native'
+import React, { useContext } from 'react'
+import { Context } from '../Helper/context'
+import { commonStyles } from '../Helper/helper';
+import ItemsList from '../Components/ItemsList';
+
+export default function Activities() {
+  // Use useContext to read the activities.
+  const { theme, activities } = useContext(Context);
+
+  return (
+    <View style={[commonStyles.topContainer, commonStyles[theme]]}>
+      <ItemsList items={activities} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
