51b6b98c987b42b3a0f5811fd40374112edaee21
Raulitoflying <1017329098@qq.com>
2024-10-24 19:24:21 -0700

    Refactor Item component to use Pressable instead of View for item container and add navigation functionality

--- Components/Item.js
+++ Components/Item.js
@@ -1,22 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { colors, commonStyles } from '../helper/helper';
+import { useNavigation } from '@react-navigation/native';
 
-export default function Item({item: {name, value, date, isSpecial}}) {
+export default function Item({item, type}) {
+  const navigation = useNavigation();
+  const {name, value, date, isSpecial} = item;
+
   return (
-    <View style={commonStyles.itemContainer}>
+    <Pressable
+      android_ripple={{color: 'white', radius: 20}}
+      style={({pressed}) => [commonStyles.itemContainer, pressed && commonStyles.pressedStyle]}
+      onPress={() => navigation.navigate(type === 'activities' ? 'EditAnActivity' : 'EditADietEntry', {item})}
+    >
       <Text style={[commonStyles.itemText, commonStyles.itemLeft]}>{name}</Text>
       <View style={commonStyles.itemRight}>
-        {isSpecial && <Ionicons name="warning" size={24} color={colors.yellow} />}
+        {isSpecial && !item.isApproved && <Ionicons name="warning" size={24} color={colors.yellow} />}
         <View style={commonStyles.textContainer}>
-            <Text style={commonStyles.itemText}>{date}</Text>
+            <Text style={commonStyles.itemText}>{new Date(date).toString().slice(0, 15)}</Text>
         </View>
         <View style={commonStyles.textContainer}>
             <Text style={commonStyles.itemText}>{value}</Text>
         </View>
       </View>
-    </View>
+    </Pressable>
   )
 }
 
