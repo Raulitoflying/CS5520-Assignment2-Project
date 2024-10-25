e6db2949993a1a879293ec28e6242ff8ce32a80f
Raulitoflying <1017329098@qq.com>
2024-10-24 19:25:04 -0700

    Refactor Diet component to use context for theme and fetch diet items from Firebase

--- Screens/Diet.js
+++ Screens/Diet.js
@@ -1,14 +1,34 @@
-import { StyleSheet, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { Context } from '../helper/context'
 import { commonStyles } from '../helper/helper';
 import ItemsList from '../Components/ItemsList';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { database } from '../Firebase/firebaseSetup';
 
-export default function Diet({ theme, diet }) {
+export default function Diet() {
+  // Use useContext to read the theme.
+  const { theme } = useContext(Context);
+  const [diet, setDiet] = useState([]);
+  
+  const collectionName = 'diet';
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(database, collectionName), (querySnapshot) => {
+      let newArray = [];
+      querySnapshot.forEach(docSnapshot => {
+        newArray.push({...docSnapshot.data(), id: docSnapshot.id});
+      })
+      setDiet(newArray);
+    })
+    return () => unsubscribe();
+  }, [])
+
   return (
     <View style={[commonStyles.topContainer, commonStyles[theme]]}>
-      <ItemsList items={diet} />
+      <ItemsList items={diet} type="diet" />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
