a70475564198d45ccbcc27ebc9937760d6d3f60f
Raulitoflying <1017329098@qq.com>
2024-10-24 19:24:50 -0700

    Refactor Activities component to use context for theme and fetch activities from Firebase

--- Screens/Activities.js
+++ Screens/Activities.js
@@ -1,14 +1,33 @@
-import { StyleSheet, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, View } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { Context } from '../helper/context'
 import { commonStyles } from '../helper/helper';
 import ItemsList from '../Components/ItemsList';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { database } from '../Firebase/firebaseSetup';
 
-export default function Activities({ theme, activities }) {
+export default function Activities() {
+  const { theme } = useContext(Context);
+  const [activities, setActivities] = useState([]);
+
+  const collectionName = 'activities';
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(database, collectionName), (querySnapshot) => {
+      let newArray = [];
+      querySnapshot.forEach(docSnapshot => {
+        newArray.push({...docSnapshot.data(), id: docSnapshot.id});
+      })
+      setActivities(newArray);
+    })
+    return () => unsubscribe();
+  }, [])
+
   return (
     <View style={[commonStyles.topContainer, commonStyles[theme]]}>
-      <ItemsList items={activities} />
+      <ItemsList items={activities} type="activities" />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
