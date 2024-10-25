9e9a2db3a4f718228d5c1b70e16564310978712f
Raulitoflying <1017329098@qq.com>
2024-10-24 19:24:30 -0700

    Refactor ItemsList component to pass item type prop to Item component

--- Components/ItemsList.js
+++ Components/ItemsList.js
@@ -3,13 +3,13 @@ import React from 'react'
 import Item from './Item'
 
 // Use the prop "item" to communicate what type of entries (diet or activity) to show.
-export default function ItemsList({items}) {
+export default function ItemsList({items, type}) {
   return (
     // FlatList has a better performance for showing a list of dynamic data than ScrollView.
     <FlatList
-        data={items}
-        renderItem={({item}) => <Item item={item} />}
-        keyExtractor={item => item.name}
+      data={items}
+      renderItem={({item}) => <Item item={item} type={type} />}
+      keyExtractor={item => item.id}
     />
   )
 }
