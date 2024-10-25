d5654805e5ca450cfd3b50e5356fe4abd3625c39
Raulitoflying <1017329098@qq.com>
2024-10-24 20:06:43 -0700

    Refactor AddAnActivity component to import Icon from 'react-native-vector-icons/MaterialIcons'

--- Screens/AddAnActivity.js
+++ Screens/AddAnActivity.js
@@ -1,14 +1,15 @@
-import { Alert, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Alert, Pressable, StyleSheet, Text, TextInput, View, icon } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
 import { commonStyles } from '../helper/helper'
 import { Context } from '../helper/context';
 import DropDownPicker from 'react-native-dropdown-picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import FormItem from '../Components/FormItem';
-import { deleteFromDB, updateInDB, writeToDB } from '../Firebase/firestoreHelper';
+import { deleteFromDB, updateInDB, writeToDB } from '../Firebase/firebaseHelper';
 import Checkbox from 'expo-checkbox';
 import PressableButton from '../Components/PressableButton';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function AddAnActivity({navigation, route}) {
   const { theme } = useContext(Context);
